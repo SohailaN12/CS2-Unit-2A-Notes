ac7669943a41a33eb2c224e9b41e36e398a874c0
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-11-04 16:03:50 +0000

    2.3 notes

--- Main.java
+++ Main.java
@@ -30,7 +30,47 @@ public class Main {
       System.out.println(y == y ); // true 
 
       // can you compare ints and bouble 
-      bouble z = 6.0;
+      double z = 6.0;
       System.out.println(x == z); // true (6 and 6.0 are the same value!)
+
+      // IF STATEMENTS: use boolean expressions to amke 
+      // DECISIONS about which code to execute 
+
+      // Determine what value or condition your choice rests on 
+      // Ex: Using the elevator at BWL depends on TIME 
+      double time = 8.5; // means 8:30 AM 
+      time = 7.75; // means 7:45 AM
+      time = 8.2; // means 8:15 AM 
+
+      // Put the CONDITION to test in parenthesis
+      if ( time < 8 ) {
+         // if statement BODY only runs if CONDITION was TRUE 
+         System.out.println("You can take the elevator!");
+      }
+      // Statement below is out of the if statement, so it'll run anyway 
+      System.out.println("The elevator is only available before 8 AM!");
+
+      // TWO-WAY SELECTION: if block + else block 
+      time =8.0;
+      if ( time < 8 ) {
+         System.out.println("Eligible for elevator!");
+   
+      }
+      else {
+         // Statement in ELSE block runs when If block is fulse
+         System.out.println("Take the stairs...");
+      }
+
+      // Another example of TWO-WAY SELECTION 
+      // in this code, using just a boolean VALUE (not an EXPRESSION)
+      boolean heads = false; // filpped tails
+      // if (true) 
+      if ( heads ) {
+         System.out.println("Flipped heads, Wear white.");
+      }
+      else {
+         System.out.println("Filpped tails! Wear black.");
+      }
+
    }
 }
